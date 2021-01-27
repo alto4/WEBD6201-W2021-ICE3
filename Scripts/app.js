369df16fad232cc4950aaba6cc0f30d2f4526163
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-01-27 09:40:21 -0500

    split contact class across multiple files

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,53 +6,6 @@
 
 "use strict";
 
-// Contact Class
-class Contact {
-  // getters and setters
-  get FullName() 
-  {
-    return this.m_fullName;
-  }
-
-  set FullName(value) 
-  {
-    this.m_fullName = value;
-  }
-
-  get ContactNumber() 
-  {
-    return this.m_contactNumber;
-  }
-
-  set ContactNumber(value) 
-  {
-    this.m_contactNumber = value;
-  }
-
-  get EmailAddress() 
-  {
-    return this.m_emailAddress;
-  }
-
-  set EmailAddress(value) 
-  {
-    this.m_emailAddress = value;
-  }
-
-  // constructor
-  constructor(fullName, contactNumber, emailAddress) 
-  {
-    this.m_fullName = fullName;
-    this.m_contactNumber = contactNumber;
-    this.m_emailAddress = emailAddress;
-  }
-  // methods
-  toString() 
-  {
-    return `Full Name     : ${this.m_fullName} \nContact Number: ${this.m_contactNumber}\nEmail Address : ${this.m_emailAddress}`;
-  }
-}
-
 (function()
 {
     function displayHome()
@@ -151,7 +104,7 @@ class Contact {
 
             let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
 
-            console.log(contact);
+            console.log(contact.toString());
         });
     }
 
